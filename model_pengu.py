0492f7753dae7840986b9e20bd1134e2c8c1d052
Madusanka Madiligama <83027416+mbabeykoon@users.noreply.github.com>
2022-03-17 23:25:39 -0500

    first commit

---
+++ model_pengu.py
@@ -0,0 +1,32 @@
+import pandas as pd
+pengu = pd.read_csv(r'D:\Intern\Penguin\penguin.csv')
+
+# Ordinal feature encoding
+
+df = pengu.copy()
+target = 'species'
+encode = ['sex','island']
+
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+    return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
+
+# Separating X and y
+X = df.drop('species', axis=1)
+Y = df['species']
+
+# Build random forest model
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+# Saving the model
+import pickle
+pickle.dump(clf, open(r'D:\Intern\Penguin\pengu.pkl', 'wb'))
